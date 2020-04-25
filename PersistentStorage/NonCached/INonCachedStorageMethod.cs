dd7a1dfa62f0ad4ec39d4c231d964494c52b6c30
Justin <27078349+jtsshieh@users.noreply.github.com>
2020-04-24 21:00:53 -0400

    Add documentation for Non-Cached Persistent Storage

--- PersistentStorage/NonCached/INonCachedStorageMethod.cs
+++ PersistentStorage/NonCached/INonCachedStorageMethod.cs
@@ -7,18 +7,58 @@
     public interface INonCachedStorageMethod<T> : IStorageMethod<T>
     {
         /// <summary>
-        /// Initialize the NonCached StorageMethod with Properties
+        /// Initialize the Non Cached StorageMethod with Properties
         /// </summary>
         /// <param name="Properties">The IProperties object to pass in</param>
         void Initialize(IProperties Properties);
 
+        /// <summary>
+        /// Set a string value
+        /// </summary>
+        /// <param name="Query">The query</param>
+        /// <param name="Value">The value to set the object as</param>
         void SetValue(string Query, string Value);
+
+        /// <summary>
+        /// Set an integer value
+        /// </summary>
+        /// <param name="Query">The query</param>
+        /// <param name="Value">The value to set the object as</param>
         void SetValue(string Query, int Value);
+
+        /// <summary>
+        /// Set a boolean value
+        /// </summary>
+        /// <param name="Query">The query</param>
+        /// <param name="Value">The value to set the object as</param>
         void SetValue(string Query, bool Value);
 
+        /// <summary>
+        /// Get a string value
+        /// </summary>
+        /// <param name="Query">The query</param>
+        /// <returns>The value as a string</returns>
         string GetString(string Query);
+
+        /// <summary>
+        /// Get an integer value
+        /// </summary>
+        /// <param name="Query">The query</param>
+        /// <returns>The value as an integer</returns>
         int GetInt(string Query);
+
+        /// <summary>
+        /// Get a boolean value
+        /// </summary>
+        /// <param name="Query">The query</param>
+        /// <returns>The value as a boolean</returns>
         bool GetBool(string Query);
+
+        /// <summary>
+        /// Get a value as an object
+        /// </summary>
+        /// <param name="Query">The query</param>
+        /// <returns>The value as an object</returns>
         object GetType(string Query);
     }
 }
--- PersistentStorage/NonCached/NonCachedPersistentStorage.cs
+++ PersistentStorage/NonCached/NonCachedPersistentStorage.cs
@@ -8,7 +8,7 @@
     {
 
         /// <summary>
-        /// Initialize the Selected Cached Persistent Storage Method
+        /// Initialize the Selected Non Cached Persistent Storage Method
         /// </summary>
         /// <returns>A boolean of whether the operation completed succesfully or failed</returns>
         public bool Initialize()
@@ -19,7 +19,7 @@
         }
 
         /// <summary>
-        /// Initialize the Selected Cache Persistent Storage Method with Properties 
+        /// Initialize the Selected Non Cached Persistent Storage Method with Properties 
         /// </summary>
         /// <param name="Properties">The IProperties object to pass in</param>
         /// <returns>A boolean of whether the operation completed succesfully or failed</returns>
@@ -30,36 +30,71 @@
             return true;
         }
 
+        /// <summary>
+        /// Set a string value
+        /// </summary>
+        /// <param name="Query">The query</param>
+        /// <param name="Value">The value to set the object as</param>
         public void SetValue(string Query, string Value)
         {
             ((INonCachedStorageMethod<T>)StorageMethod).SetValue(Query, Value);
         }
 
+        /// <summary>
+        /// Set an integer value
+        /// </summary>
+        /// <param name="Query">The query</param>
+        /// <param name="Value">The value to set the object as</param>
         public void SetValue(string Query, int Value)
         {
             ((INonCachedStorageMethod<T>)StorageMethod).SetValue(Query, Value);
         }
 
+        /// <summary>
+        /// Set a boolean value
+        /// </summary>
+        /// <param name="Query">The query</param>
+        /// <param name="Value">The value to set the object as</param>
         public void SetValue(string Query, bool Value)
         {
             ((INonCachedStorageMethod<T>)StorageMethod).SetValue(Query, Value);
         }
        
+        /// <summary>
+        /// Get a string value
+        /// </summary>
+        /// <param name="Query">The query</param>
+        /// <returns>The value as a string</returns>
         public string GetString(string Query)
         {
             return ((INonCachedStorageMethod<T>)StorageMethod).GetString(Query);
         }
 
+        /// <summary>
+        /// Get an integer value
+        /// </summary>
+        /// <param name="Query">The query</param>
+        /// <returns>The value as an integer</returns>
         public int GetInt(string Query)
         {
             return ((INonCachedStorageMethod<T>)StorageMethod).GetInt(Query);
         }
 
+        /// <summary>
+        /// Get a boolean value
+        /// </summary>
+        /// <param name="Query">The query</param>
+        /// <returns>The value as a boolean</returns>
         public bool GetBool(string Query)
         {
             return ((INonCachedStorageMethod<T>)StorageMethod).GetBool(Query);
         }
 
+        /// <summary>
+        /// Get a value as an object
+        /// </summary>
+        /// <param name="Query">The query</param>
+        /// <returns>The value as an object</returns>
         public object GetType(string Query)
         {
             return ((INonCachedStorageMethod<T>)StorageMethod).GetType(Query);
